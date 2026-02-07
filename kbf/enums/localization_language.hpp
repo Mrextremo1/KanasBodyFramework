001fc1a98145dd499bba842b0ab8fc018590c0e1
kana-desu <kana.no.kanae.ten@gmail.com>
2026-02-07 00:22:51 +0000

    Localization enum

---
+++ kbf/enums/localization_language.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace kbf {
+
+	// Add more languages here from via.Language if you need
+	enum class LocalizationLanguage {
+		Invalid  = -1,
+		Japanese = 0,
+		English  = 1
+	};
+
+}
